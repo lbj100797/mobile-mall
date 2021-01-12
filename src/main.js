591376c67d8bdf8d6fa6616548ffe34fb35e7baf
gao <1344690411@qq.com>
2021-01-12 18:41:10 +0800

    update

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import VueLazyLoad from 'vue-lazyload'
+
+Vue.config.productionTip = false
+
+Vue.use(VueLazyLoad, {
+  preLoad: 1,
+})
+
+new Vue({
+  render: h => h(App),
+  store,
+  router
+}).$mount('#app')
